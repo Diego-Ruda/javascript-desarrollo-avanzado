1dd983025094e97a5b0fa9dd3358bcf93917fe38
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-27 16:59:42 -0300

    Seleccion de elementos de DOM

--- clase01/02.dom-bom/src/main.js
+++ clase01/02.dom-bom/src/main.js
@@ -12,3 +12,39 @@ console.log(window)
 
 // Javascript no es ECMAScript -> Define el lenhuaje Javascript
 // Que el navegador agregar (APIs) -> BOM -> Fetch, Geolocation , HTMLCollection
+
+console.log(window.location.href)
+
+const btnIr = document.getElementById('btn-ir')
+
+console.log(btnIr)
+
+btnIr.addEventListener('click', function() {
+  window.location.href= 'https://google.com'
+})
+
+
+console.warn('// ! Seleccion de elementos en DOM')
+
+console.warn('Tradicionales')
+
+// * getElementById
+// * getElementByClassName
+// * getElementByTagName
+
+console.warn('Modernos')
+
+// * querySelector
+// * querySelectorAll
+
+// utilizo todos lo selectores que vengo utilizando en CSS con querySelector
+
+// Si quiero seleccionar un ID
+// Si quiero selecionar un CLASE
+// Si quiero selecionar un TAG
+
+// const tituloPrincipal = document.querySelector ('h1)
+// const tituloPrincipal = document.querySelector('.text-2xl.text-green-600')
+const tituloPrincipal = document.querySelector('#titulo-principal')
+
+console.log(tituloPrincipal)
