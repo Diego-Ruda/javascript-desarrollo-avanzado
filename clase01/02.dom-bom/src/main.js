4ade21b43cf185b0fa82ed60fed1de2f3c7b3e3c
Diego Ruda <diegorudaamd18@gmail.com>
2026-05-27 16:38:16 -0300

    Empezando con DOM y BOM

---
+++ clase01/02.dom-bom/src/main.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+
+console.dir(document)
+console.warn('// ! BOM - Browser Object Model')
+
+console.log(window)
+
+// ECAMAscript genrea los estandares para que Javascript se puede utilizar en todos los navegadores  y en el nackenmd de la misma manera
+
+// https://ecma-international.org/
+
+// Javascript no es ECMAScript -> Define el lenhuaje Javascript
+// Que el navegador agregar (APIs) -> BOM -> Fetch, Geolocation , HTMLCollection
