3984468a26bd28c5777a3738034eea117132997f
Diego Ruda <diegorudaamd18@gmail.com>
2026-06-01 17:00:03 -0300

    Explicacion didactica sobre promesas

--- clase03/01.peticiones-asincronicas-ajax/src/main.js
+++ clase03/01.peticiones-asincronicas-ajax/src/main.js
@@ -64,3 +64,86 @@ console.warn('/* Comunicación asincrónica con AJAX */')
 // POST -> CRUD -> C:CREATE -> http://localhost:8080/productos + el nuevo producto
 // PUT -> CRUD -> U:UPDATE -> http://localhost:8080/productos/id + el producto editado
 // DELETE -> CRUD -> D:DELETE -> http://localhost:8080/productos/id
+
+// console.log('Inicio')
+// console.log('Tarea/Proceso 1'); for (let index = 0; index < 1e10; index++) {} // 10_000_000_000
+// console.log('Tarea/Proceso 2'); for (let index = 0; index < 1e10; index++) {} // 10_000_000_000
+// console.log('Tarea/Proceso 3'); for (let index = 0; index < 1e10; index++) {} // 10_000_000_000
+// console.log('Tarea/Proceso 4'); for (let index = 0; index < 1e10; index++) {} // 10_000_000_000
+// console.log('Tarea/Proceso 5'); for (let index = 0; index < 1e10; index++) {} // 10_000_000_000
+// console.log('Tarea/Proceso 6'); for (let index = 0; index < 1e10; index++) {} // 10_000_000_000
+// console.log('Fin')
+
+
+// Asincronico -> No secuencial
+
+// console.log('Inicio')
+// setTimeout(()=>{
+//   console.log('Se muestra luego de 15 segundos')
+// },15000)
+// console.log('Fin')
+
+
+
+// setTimeout(createBrotliCompress, milisegundos)
+
+// console.log(Inicio)
+// setTimeout(()=>{
+//   console.log('En 5 segundos voy a aparecer...')
+//   console.log(new Date().toLocaleTimeString())
+//   console.log('Fin')
+// }, 5000) // 5 segundos
+
+// Callback hell -> Piramide de la perdicion -> Aparece primesas por este incoveniente
+
+console.warn('//! Promises')
+// Las promesas nos ayudan a gestionar de una manera mas amable la asincronia Remplza a los callbacks
+//Las priomesas estan dentro del lenguaje de Javascript
+
+//una promesa tiene tres estados
+// 1. Pendiente(pending): esta en proceso
+// 2. Se cumplio (fulfilled): se resolvio existosamente
+// 3. No se cumplio -> Rechazada (rejected): ocurrio un error
+
+
+//------------------------------------------------------------------------
+
+//Promise(cb) <-------- una funcion ejecutada pasada como argumento de otra
+// resolve y reject son referencias de funciones
+// resolve -> Se usa cuando quiero que se cumpla la promesa
+// reject -> Se usa cuando quiero que no se cumpla promesa
+
+// const promesa = new Promise(function(resolve, reject) {
+//   console.log(resolve) // funciones que entrega el Promise cuando lo instacio
+//   console.log(reject) // funciones que entrega el Promise cuando lo instacio
+// })
+// console.log(promesa)
+
+// ! una promesa que nunca se cumple
+// const promesa = new Promise(function(resolve, reject){
+//   reject()
+// })
+
+// console.log(promesa)
+
+//! Una promesa que siempre se cumple
+
+// const promesa = new Promise(function(resolve, reject){
+//   reject()
+// })
+
+// console.log(promesa)
+
+const cumpleBeta = false
+
+const promesa = new promesa(function(resolve,reject){
+  setTimeout(()=>{
+    if(cumpleBeta){
+      resolve('Fui al cumple. Cumpli')
+    }else{
+      reject('No fui al cumple. Y no ser me cae la cara de verguenza')
+    }
+  },10000) // 10 segundos
+})
+
+console.log(promesa)
