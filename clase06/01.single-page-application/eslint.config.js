43d313de7d98384982a4d0f198d4b41f80c889c3
Diego Ruda <diegorudaamd18@gmail.com>
2026-06-08 15:33:42 -0300

    Empezando con SPA y configurando herramientas eslint, prettier, editorconfig

---
+++ clase06/01.single-page-application/eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin, { languages } from 'eslint-plugin-prettier';
+export default [
+  js.configs.recommended,
+  {
+    languagesOptions:{
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      }
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettier.rules,
+      // calidad
+      'no-unused-vars': 'warn',
+      // 'no-console': 'warn',
+      eqeqeq: 'error',
+      // buenas prácticas
+      'prefer-const': 'error',
+      'no-var': 'error',
+      // prettier como fuente única de formato
+      'prettier/prettier': 'error',
+    },
+  },
+];
---
+++ clase06/01.single-page-application/src/main.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+let apellido = 'Ruda';
+let nombre = 'Diego';
+
+console.log(nombre);
+console.log(apellido);
+
+var total = 0;
+
+function sumar(a, b) {
+  const resultado = a + b;
+  return resultado;
+}
+
+if (total === '0') {
+  console.log('Total es cero');
+}
+
+sumar(2, 5);
