6df431bcbaa4135fe4e9394094247d4513196bf5
Diego Ruda <diegorudaamd18@gmail.com>
2026-06-17 15:32:23 -0300

    selección y agrego un evento al input file

--- clase09/01.ajax-binario.drag-and-drop/src/main.js
+++ clase09/01.ajax-binario.drag-and-drop/src/main.js
@@ -1,2 +1,11 @@
-import './style.css'
+import './style.css';
 
+// evento
+// input files
+const inputFile = document.querySelector('#input-file');
+
+inputFile.addEventListener('change', (e) => {
+  console.log('cambio el input');
+  const archivo = e.target.files[0];
+  console.dir(archivo);
+});
--- clase09/01.ajax-binario.drag-and-drop/vite.config.js
+++ clase09/01.ajax-binario.drag-and-drop/vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  css:  {
-    devSourcemap:true   
-  }
-})
+  plugins: [tailwindcss()],
+  css: {
+    devSourcemap: true,
+  },
+});
